ed4fd4e0285f2fa0aead8c31be4d3efb14c9c0bd
Muhammad Farhan N. A <mfna1998@gmail.com>
2020-02-24 10:04:12 +0700

    update pasien

--- application/views/pasien/pasien_list.php
+++ application/views/pasien/pasien_list.php
@@ -43,28 +43,15 @@
         </div>
         <table class="table table-bordered" style="margin-bottom: 10px">
             <tr>
-                <th>No</th>
+				<th>No</th>
+		<th>NORM</th>
 		<th>NAMA</th>
 		<th>PANGGILAN</th>
 		<th>GELAR DEPAN</th>
 		<th>GELAR BELAKANG</th>
-		<th>TEMPAT LAHIR</th>
 		<th>TANGGAL LAHIR</th>
 		<th>JENIS KELAMIN</th>
-		<th>ALAMAT</th>
-		<th>RT</th>
-		<th>RW</th>
-		<th>KODEPOS</th>
-		<th>WILAYAH</th>
-		<th>AGAMA</th>
-		<th>PENDIDIKAN</th>
-		<th>PEKERJAAN</th>
-		<th>STATUS PERKAWINAN</th>
-		<th>GOLONGAN DARAH</th>
 		<th>KEWARGANEGARAAN</th>
-		<th>SUKUBANGSA</th>
-		<th>BAHASA</th>
-		<th>LINGKUNGANKERJA</th>
 		<th>TUJUANPERIKSA</th>
 		<th>JENIS PASIEN</th>
 		<th>CEKNIK</th>
@@ -77,27 +64,14 @@
                 ?>
                 <tr>
 			<td width="80px"><?php echo ++$start ?></td>
+			<td><?php echo $pasien->NORM ?></td>
 			<td><?php echo $pasien->NAMA ?></td>
 			<td><?php echo $pasien->PANGGILAN ?></td>
 			<td><?php echo $pasien->GELAR_DEPAN ?></td>
 			<td><?php echo $pasien->GELAR_BELAKANG ?></td>
-			<td><?php echo $pasien->TEMPAT_LAHIR ?></td>
 			<td><?php echo $pasien->TANGGAL_LAHIR ?></td>
 			<td><?php echo $pasien->JENIS_KELAMIN ?></td>
-			<td><?php echo $pasien->ALAMAT ?></td>
-			<td><?php echo $pasien->RT ?></td>
-			<td><?php echo $pasien->RW ?></td>
-			<td><?php echo $pasien->KODEPOS ?></td>
-			<td><?php echo $pasien->WILAYAH ?></td>
-			<td><?php echo $pasien->AGAMA ?></td>
-			<td><?php echo $pasien->PENDIDIKAN ?></td>
-			<td><?php echo $pasien->PEKERJAAN ?></td>
-			<td><?php echo $pasien->STATUS_PERKAWINAN ?></td>
-			<td><?php echo $pasien->GOLONGAN_DARAH ?></td>
 			<td><?php echo $pasien->KEWARGANEGARAAN ?></td>
-			<td><?php echo $pasien->SUKUBANGSA ?></td>
-			<td><?php echo $pasien->BAHASA ?></td>
-			<td><?php echo $pasien->LINGKUNGANKERJA ?></td>
 			<td><?php echo $pasien->TUJUANPERIKSA ?></td>
 			<td><?php echo $pasien->JENIS_PASIEN ?></td>
 			<td><?php echo $pasien->CEKNIK ?></td>
