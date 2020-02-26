4e91426788073ffd21c51b0e9bbc6d74151ac544
Muhammad Farhan N. A <mfna1998@gmail.com>
2020-02-26 10:02:37 +0700

    perubahan

--- application/views/jenis_referensi/jenis_referensi_list.php
+++ application/views/jenis_referensi/jenis_referensi_list.php
@@ -9,11 +9,12 @@
 
         <title>Jenis Referensi</title>
 
+        <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>"/>
         <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
         <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
         <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
 
-        <link href="vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+        <!-- <link href="vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all"> -->
 
         <link href="vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
         <link href="vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
